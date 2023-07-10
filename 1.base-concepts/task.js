582f229df860484633c0933cf725df7d07ec4086
Djama <solverm85@gmail.com>
2023-07-10 14:32:08 +0300

    Added first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,16 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d < 0) {
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } 
+
   
   return arr;
 }
@@ -8,3 +18,22 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
+
+"use strict";
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+
+  let creditBody = amount - contribution;
+  let monthlyPercent = percent / 100 / 12;
+
+  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1))); // ежемесячный платеж
+
+  let totalAmount = (monthlyPayment * countMonths).toFixed(2);
+
+  return Number(totalAmount);
+}
+
+
+console.log(calculateTotalMortgage(10, 10000, 50000, 12)); 
