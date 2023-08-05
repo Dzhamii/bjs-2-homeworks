bb452f35e3699b6b35d372b8ce1de5d912b21c51
Djama <solverm85@gmail.com>
2023-08-05 23:56:15 +0300

    Added objects task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,43 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
   
+    marks = [];
 }
+
+let student1 = new Student("Alex", "male", 27);
+let student2 = new Student("Samanta", "female", 24);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.excluded) {
+        this.marks.push(...marksToAdd);
+  } 
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks && this.marks.length > 0) {
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.subject = null;
+  this.marks = [];
+  this.excluded = reason;  
 }
+
+student1.setSubject("Algebra"); 
+console.log(student1.getAverage());
+student1.addMarks(4,5,4,5);
+console.log(student1);
+
+
