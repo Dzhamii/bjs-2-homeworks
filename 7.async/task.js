1ee952ccc0ae19b47e1567d56dcd9c70ec2f1996
Djama <solverm85@gmail.com>
2023-08-16 00:25:17 +0300

    Added commit async

---
+++ 7.async/task.js
@@ -0,0 +1,57 @@
+class AlarmClock {
+    constructor() {
+      this.alarmCollection = [];
+      this.intervalId = null;
+    }
+  
+    addClock(time, callback) {
+      if (!time || !callback) {
+        throw new Error("Отсутствуют обязательные аргументы");
+      }
+  
+      if (this.alarmCollection.some(alarm => alarm.time === time)) {
+        console.warn("Уже присутствует звонок на это же время");
+        return;
+      }
+  
+      this.alarmCollection.push({ callback, time, canCall: true });
+    }
+  
+    removeClock(time) {
+      this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+  
+    getCurrentFormattedTime() {
+      const now = new Date();
+      return `${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`;
+    }
+  
+    start() {
+      if (this.intervalId === null) {
+        this.intervalId = setInterval(() => this.alarmCollection.forEach(alarm => {
+          if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
+          }
+        }), 1000);
+      }
+    }
+  
+    stop() {
+      if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+      }
+    }
+  
+    resetAllCalls() {
+      this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+  
+    clearAlarms() {
+      this.stop();
+      this.alarmCollection = [];
+    }
+  }
+  
+  
